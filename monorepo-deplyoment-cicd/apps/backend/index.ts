1f3dc8a632f7b212fea9d8a565f4ac6cc96ac464
karishma9142 <karishmacair47050@gmail.com>
2026-07-14 20:13:45 +0530

    add frontend , backend and prisma client

---
+++ monorepo-deplyoment-cicd/apps/backend/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import { prisma } from 'db/client'
+
+const app = express();
+console.log(process.env.DATABASE_URL);
+app.post("/users", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            res.status(400).json({
+                error: "username and password required"
+            })
+        }
+        const user = await prisma.user.create({
+            data: {
+                username,
+                password
+            }
+        })
+
+        res.status(200).json({
+            msg: "user created"
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg: "internal server error"
+        })
+    }
+})
+
+app.get("/users", async (req, res) => {
+    try {
+        const data = await prisma.user.findMany();
+        res.status(200).json({
+            data : data
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg: "internal server error"
+        })
+    }
+})
+
+app.listen(3000 , ()=> {
+    console.log("server running on port 3000")
+})
---
+++ monorepo-deplyoment-cicd/apps/web/app/page.tsx
@@ -0,0 +1,18 @@
+import { prisma } from "db/client";
+
+export default async function Home() {
+  console.log(process.env.DATABASE_URL);
+  await prisma.user.create({
+    data : {
+      username : "karishma",
+      password : "1234"
+    }
+  })
+  const users = await prisma.user.findMany();
+
+  return (
+    <pre>
+      {JSON.stringify(users, null, 2)}
+    </pre>
+  );
+}
